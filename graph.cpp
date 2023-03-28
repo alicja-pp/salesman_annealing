bdbd574dd82205cbd6094c000aff7e0078c83002
Dawid Karp <dadulerk@gmail.com>
2023-03-28 20:54:53 +0200

    format file, include ROOT h-files

--- graph.cpp
+++ graph.cpp
@@ -1,12 +1,16 @@
-#include <stdio.h>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
-#include <cstdlib>
 #include <algorithm>
 #include <chrono>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
 #include <random>
+#include <sstream>
+#include <stdio.h>
+#include <vector>
+
+#include "TSystem.h"
+#include "TCanvas.h"
+#include "TGraph.h"
 
 using namespace std;
 
@@ -24,27 +28,31 @@ struct State {
 
 double total_distance(vector<int> idx, vector<City> cities) {
     long double d = 0;
-    for (int i = 1; i < idx.size(); i++){
-        d += sqrt(pow(cities[idx[i]].x - cities[idx[i - 1]].x, 2) + pow(cities[idx[i]].y - cities[idx[i - 1]].y, 2));
+    for (int i = 1; i < idx.size(); i++) {
+        d += sqrt(pow(cities[idx[i]].x - cities[idx[i - 1]].x, 2) +
+                  pow(cities[idx[i]].y - cities[idx[i - 1]].y, 2));
     }
-    d += sqrt(pow(cities[idx.size()-1].x - cities[0].x, 2) + pow(cities[idx.size()-1].y - cities[0].y, 2));
+    d += sqrt(pow(cities[idx.size() - 1].x - cities[0].x, 2) +
+              pow(cities[idx.size() - 1].y - cities[0].y, 2));
     return d;
 }
 
-vector<double> get_x(vector<City> cities){
+vector<double> get_x(vector<City> cities) {
     vector<double> xs(cities.size());
-    transform(cities.begin(), cities.end(), xs.begin(), [](City const& city) { return city.x; });
+    transform(cities.begin(), cities.end(), xs.begin(),
+              [](City const &city) { return city.x; });
     return xs;
 }
 
-vector<double> get_y(vector<City> cities){
+vector<double> get_y(vector<City> cities) {
     vector<double> ys(cities.size());
-    transform(cities.begin(), cities.end(), ys.begin(), [](City const& city) { return city.y; });
+    transform(cities.begin(), cities.end(), ys.begin(),
+              [](City const &city) { return city.y; });
     return ys;
 }
 
-void visualize(vector<int> indices, vector<City> cities){
-    //wizualizacja drogi między miastami na bieżąco
+void visualize(vector<int> indices, vector<City> cities) {
+    // wizualizacja drogi między miastami na bieżąco
     TCanvas *c = new TCanvas("c", "Path", 1000, 800);
     vector<double> xs = get_x(cities);
     vector<double> ys = get_y(cities);
@@ -56,10 +64,11 @@ void visualize(vector<int> indices, vector<City> cities){
 
     TGraph *g = new TGraph(1, &cities[0].x, &cities[0].y);
 
-    for(int i=0; i<indices.size(); i++){
+    for (int i = 0; i < indices.size(); i++) {
         g->SetPoint(i, cities[indices.at(i)].x, cities[indices.at(i)].y);
     }
-    g->SetPoint(indices.back(), cities[indices.at(0)].x, cities[indices.at(0)].y);
+    g->SetPoint(indices.back(), cities[indices.at(0)].x,
+                cities[indices.at(0)].y);
     g1->Draw("AP");
     g->Draw("same");
     c->Modified();
@@ -67,7 +76,7 @@ void visualize(vector<int> indices, vector<City> cities){
     gSystem->ProcessEvents();
 }
 
-vector<City> load_cities(string filename){
+vector<City> load_cities(string filename) {
     ifstream Cities;
     Cities.open(filename);
 
@@ -76,11 +85,10 @@ vector<City> load_cities(string filename){
     double index, x, y;
     string coo;
 
-    while(getline(Cities, coo)) {
+    while (getline(Cities, coo)) {
         stringstream line(coo);
 
-        if (line >> index >> x >> y)
-            cities.push_back(City {x,y});
+        if (line >> index >> x >> y) cities.push_back(City{x, y});
     }
 
     Cities.close();
@@ -88,26 +96,24 @@ vector<City> load_cities(string filename){
     return cities;
 }
 
-void graph(){
-    //czytanie współrzędnych miast z pliku, zapis do wektorów
+void graph(void) {
+    // czytanie współrzędnych miast z pliku, zapis do wektorów
     vector<City> cities = load_cities("ireland-5.txt");
 
     vector<int> indices(cities.size());
     iota(begin(indices), end(indices), 0);
 
-    State state = State {
-        .temperature = 1,
-        .total_length = total_distance(indices, cities),
-        indices,
-        cities
-    };
-    
-    //losujemy kolejność miast
+    State state = State{.temperature = 1,
+                        .total_length = total_distance(indices, cities),
+                        .indices = indices,
+                        .cities = cities};
+
+    // losujemy kolejność miast
     srand(time(NULL));
     unsigned seed = chrono::system_clock::now().time_since_epoch().count();
-    shuffle(indices.begin(), indices.end(),  default_random_engine(seed));
-    
-    //visualize(state.indices, state.cities);
+    shuffle(indices.begin(), indices.end(), default_random_engine(seed));
+
+    // visualize(state.indices, state.cities);
     TCanvas *c = new TCanvas("c", "Path", 1000, 800);
     vector<double> xs = get_x(cities);
     vector<double> ys = get_y(cities);
@@ -119,22 +125,27 @@ void graph(){
 
     TGraph *g = new TGraph(1, &cities[0].x, &cities[0].y);
 
-    for (int i=0; i<indices.size(); i++) {
+    for (int i = 0; i < indices.size(); i++)
         g->SetPoint(i, cities[indices.at(i)].x, cities[indices.at(i)].y);
-    }
-    g->SetPoint(indices.size(), cities[indices.at(0)].x, cities[indices.at(0)].y);
+
+    g->SetPoint(indices.size(), cities[indices.at(0)].x,
+                cities[indices.at(0)].y);
+
     g1->Draw("AP");
     g->Draw("same");
     c->Modified();
     c->Update();
     gSystem->ProcessEvents();
-    
+
     while (1) {
-        shuffle(indices.begin(), indices.end(),  default_random_engine(seed));
-        for (int i=0; i<indices.size(); i++) {
+        shuffle(indices.begin(), indices.end(), default_random_engine(seed));
+
+        for (int i = 0; i < indices.size(); i++)
             g->SetPoint(i, cities[indices.at(i)].x, cities[indices.at(i)].y);
-        }
-        g->SetPoint(indices.size(), cities[indices.at(0)].x, cities[indices.at(0)].y);
+
+        g->SetPoint(indices.size(), cities[indices.at(0)].x,
+                    cities[indices.at(0)].y);
+
         g1->Draw("AP");
         g->Draw("same");
         c->Modified();
@@ -143,3 +154,10 @@ void graph(){
         sleep(1);
     }
 }
+
+#ifndef __CINT__
+int main() {
+    graph();
+    return 0;
+}
+#endif
