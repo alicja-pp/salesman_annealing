cd9b92de0474b86e05026d67c00fe37de9000a3a
Alicja Płachta <alicjaplachta@MacBook-Air-Alicja.local>
2023-03-28 17:01:42 +0200

    visualize closed paths

--- graph.cpp
+++ graph.cpp
@@ -15,7 +15,14 @@ struct City {
     double y;
 };
 
-double count_distance(vector<int> idx, vector<City> cities) {
+struct State {
+    double temperature;
+    double total_length;
+    vector<int> indices;
+    vector<City> cities;
+};
+
+double total_distance(vector<int> idx, vector<City> cities) {
     long double d = 0;
     for (int i = 1; i < idx.size(); i++){
         d += sqrt(pow(cities[idx[i]].x - cities[idx[i - 1]].x, 2) + pow(cities[idx[i]].y - cities[idx[i - 1]].y, 2));
@@ -38,11 +45,11 @@ vector<double> get_y(vector<City> cities){
 
 void visualize(vector<int> indices, vector<City> cities){
     //wizualizacja drogi między miastami na bieżąco
-    TCanvas *c = new TCanvas("c", "Ireland", 1000, 800);
+    TCanvas *c = new TCanvas("c", "Path", 1000, 800);
     vector<double> xs = get_x(cities);
     vector<double> ys = get_y(cities);
     TGraph *g1 = new TGraph(indices.size(), &xs[0], &ys[0]);
-    g1->SetTitle("Ireland");
+    g1->SetTitle("Path");
     g1->SetMarkerColor(9);
     g1->SetMarkerStyle(29);
     g1->SetMarkerSize(1);
@@ -51,12 +58,13 @@ void visualize(vector<int> indices, vector<City> cities){
 
     for(int i=0; i<indices.size(); i++){
         g->SetPoint(i, cities[indices.at(i)].x, cities[indices.at(i)].y);
-        g1->Draw("AP");
-        g->Draw("same");
-        c->Modified();
-        c->Update();
-        gSystem->ProcessEvents();
     }
+    g->SetPoint(indices.back(), cities[indices.at(0)].x, cities[indices.at(0)].y);
+    g1->Draw("AP");
+    g->Draw("same");
+    c->Modified();
+    c->Update();
+    gSystem->ProcessEvents();
 }
 
 vector<City> load_cities(string filename){
@@ -82,21 +90,56 @@ vector<City> load_cities(string filename){
 
 void graph(){
     //czytanie współrzędnych miast z pliku, zapis do wektorów
-    vector<City> cities = load_cities("ireland.txt");
+    vector<City> cities = load_cities("ireland-5.txt");
 
     vector<int> indices(cities.size());
     iota(begin(indices), end(indices), 0);
-    cout<< endl<<indices.back()<<endl;
+
+    State state = State {
+        .temperature = 1,
+        .total_length = total_distance(indices, cities),
+        indices,
+        cities
+    };
+    
     //losujemy kolejność miast
     srand(time(NULL));
     unsigned seed = chrono::system_clock::now().time_since_epoch().count();
     shuffle(indices.begin(), indices.end(),  default_random_engine(seed));
+    
+    //visualize(state.indices, state.cities);
+    TCanvas *c = new TCanvas("c", "Path", 1000, 800);
+    vector<double> xs = get_x(cities);
+    vector<double> ys = get_y(cities);
+    TGraph *g1 = new TGraph(indices.size(), &xs[0], &ys[0]);
+    g1->SetTitle("Path");
+    g1->SetMarkerColor(9);
+    g1->SetMarkerStyle(29);
+    g1->SetMarkerSize(1);
 
-    //odległość
-    double distance = count_distance(indices, cities);
+    TGraph *g = new TGraph(1, &cities[0].x, &cities[0].y);
 
-    cout.precision(17);
-    cout << distance << endl;
-
-    visualize(indices, cities);
+    for (int i=0; i<indices.size(); i++) {
+        g->SetPoint(i, cities[indices.at(i)].x, cities[indices.at(i)].y);
+    }
+    g->SetPoint(indices.size(), cities[indices.at(0)].x, cities[indices.at(0)].y);
+    g1->Draw("AP");
+    g->Draw("same");
+    c->Modified();
+    c->Update();
+    gSystem->ProcessEvents();
+    
+    while (1) {
+        shuffle(indices.begin(), indices.end(),  default_random_engine(seed));
+        for (int i=0; i<indices.size(); i++) {
+            g->SetPoint(i, cities[indices.at(i)].x, cities[indices.at(i)].y);
+        }
+        g->SetPoint(indices.size(), cities[indices.at(0)].x, cities[indices.at(0)].y);
+        g1->Draw("AP");
+        g->Draw("same");
+        c->Modified();
+        c->Update();
+        gSystem->ProcessEvents();
+        sleep(1);
+    }
 }
