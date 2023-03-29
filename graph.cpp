5c25be3dfd31a4837c75d6f7bf083951d74a53ba
Dawid Karp <dadulerk@gmail.com>
2023-03-29 13:23:37 +0200

    calculate acceptance probability, draw length (energy) over time

--- graph.cpp
+++ graph.cpp
@@ -20,20 +20,22 @@ struct City {
 };
 
 struct State {
+    long step;
     double temperature;
     double total_length;
     vector<int> indices;
-    vector<City> cities;
+    const vector<City> cities;
 };
 
-double total_distance(vector<int> idx, vector<City> cities) {
+double total_distance(vector<City> cities, vector<int> idx) {
     long double d = 0;
-    for (int i = 0; i < idx.size(); i++) {
-        d += sqrt(pow(cities[idx[i + 1]].x - cities[idx[i]].x, 2) +
-                  pow(cities[idx[i + 1]].y - cities[idx[i]].y, 2));
+
+    for (int i = 0; i < idx.size() - 1; i++) {
+        d += pow(cities[idx[i + 1]].x - cities[idx[i]].x, 2) +
+             pow(cities[idx[i + 1]].y - cities[idx[i]].y, 2);
     }
-    d += sqrt(pow(cities[idx[0]].x - cities[idx.back()].x, 2) +
-              pow(cities[idx[0]].y - cities[idx.back()].y, 2));
+    d += pow(cities[idx[0]].x - cities[idx.back()].x, 2) +
+         pow(cities[idx[0]].y - cities[idx.back()].y, 2);
     return d;
 }
 
@@ -88,15 +90,16 @@ State init_state(string filename) {
     vector<int> indices(cities.size());
     iota(begin(indices), end(indices), 0);
 
-    return State{.temperature = 1,
-                 .total_length = total_distance(indices, cities),
+    return State{.step = 0,
+                 .temperature = 1,
+                 .total_length = total_distance(cities, indices),
                  .indices = indices,
                  .cities = cities};
 }
 
 // TODO: update only changed points
 void update_graph(TCanvas *canvas, TGraph *points, TGraph *lines,
-                  State &state) {
+                  TGraph *lengths, State &state) {
     for (int i = 0; i < state.indices.size(); i++) {
         lines->SetPoint(i, state.cities[state.indices[i]].x,
                         state.cities[state.indices[i]].y);
@@ -105,8 +108,15 @@ void update_graph(TCanvas *canvas, TGraph *points, TGraph *lines,
     lines->SetPoint(state.indices.size(), state.cities[state.indices[0]].x,
                     state.cities[state.indices[0]].y);
 
+    lengths->SetPoint(state.step, state.step, state.total_length);
+
+    canvas->cd(1);
     points->Draw("AP");
     lines->Draw("same");
+
+    canvas->cd(2);
+    lengths->Draw();
+
     canvas->Modified();
     canvas->Update();
     gSystem->ProcessEvents();
@@ -114,7 +124,8 @@ void update_graph(TCanvas *canvas, TGraph *points, TGraph *lines,
 
 void graph(void) {
     // czytanie współrzędnych miast z pliku, zapis do wektorów
-    State state = init_state("ireland-5.txt");
+    State state = init_state("ireland-100.txt");
+    int step_number = 10000000;
 
     srand(time(NULL));
     unsigned seed = chrono::system_clock::now().time_since_epoch().count();
@@ -123,6 +134,7 @@ void graph(void) {
 
     // visualize(state.indices, state.cities);
     TCanvas *canvas = new TCanvas("c", "Path", 1000, 800);
+    canvas->Divide(2, 1);
 
     vector<double> xs = get_x(state.cities);
     vector<double> ys = get_y(state.cities);
@@ -136,20 +148,50 @@ void graph(void) {
 
     TGraph *lines = new TGraph(1, &state.cities[0].x, &state.cities[0].y);
 
-    update_graph(canvas, points, lines, state);
+    TGraph *lengths = new TGraph(1, 0, state.total_length);
+    lengths->SetTitle("Length");
 
-    while (1) {
-        auto [idx1, idx2] = generate_random_indices(state.cities.size() - 1);
+    update_graph(canvas, points, lines, lengths, state);
 
-        for (int i = 0; i < state.indices.size(); i++) {
-            cout << state.indices[i] << " ";
+    int rand_idx1, rand_idx2;
+
+    while (state.step < step_number) {
+        do {
+            rand_idx1 = rand() % (state.cities.size());
+            rand_idx2 = rand() % (state.cities.size());
+        } while (rand_idx1 == rand_idx2);
+
+        // auto [idx1, idx2] = generate_random_indices(state.cities.size() - 1);
+        cout << "random_idx1: " << rand_idx1 << ", "
+             << "random_idx2: " << rand_idx2 << endl;
+
+        vector<int> new_indices(state.indices);
+
+        swap(new_indices[rand_idx1], new_indices[rand_idx2]);
+
+        double new_length = total_distance(state.cities, new_indices);
+
+        double prob =
+            exp(-(new_length - state.total_length) / state.temperature);
+        double random = (double)rand() / (RAND_MAX + 1.0);
+
+        cout << "random: " << random << endl;
+        cout << "probability: " << prob << endl;
+
+        if (random < prob) {
+            state.total_length = new_length;
+            state.indices = new_indices;
         }
+
+        cout << "length: " << state.total_length << endl;
+        cout << "temperature: " << state.temperature << endl;
+
+        state.temperature *= 0.98;
+
+        update_graph(canvas, points, lines, lengths, state);
+        // sleep(1);
         cout << endl;
-
-        swap(state.indices[idx1], state.indices[idx2]);
-
-        update_graph(canvas, points, lines, state);
-        sleep(1);
+        state.step++;
     }
 }
 
